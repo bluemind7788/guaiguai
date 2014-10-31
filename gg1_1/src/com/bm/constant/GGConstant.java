374acf44f9010a710afb908d0441171cafbb4600
bluemind7788 <669380499@qq.com>
2014-10-31 21:17:55 +0800

    change paths

---
+++ gg1_1/src/com/bm/constant/GGConstant.java
@@ -0,0 +1,6 @@
+package com.bm.constant;
+
+public class GGConstant {
+	public static final String INTENT_RECOG_SERVICE = "com.bm.gg.RecogService";
+	public static final String INTENT_SOUND_SERVICE = "com.bm.gg.SoundService";
+}
